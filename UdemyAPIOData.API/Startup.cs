803c09306f6807c4c81aa61ef0d924ca139c4ed8
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-04-23 04:22:37 +0300

    OData Query Option($orderby)

--- UdemyAPIOData.API/Startup.cs
+++ UdemyAPIOData.API/Startup.cs
@@ -62,7 +62,7 @@ namespace UdemyAPIOData.API
             app.UseEndpoints(endpoints =>
             {
                 // www.api.com/odata/products
-                endpoints.Select().Expand();
+                endpoints.Select().Expand().OrderBy();
                 endpoints.MapODataRoute("odata", "odata", builder.GetEdmModel());
                 endpoints.MapControllers();
             });
